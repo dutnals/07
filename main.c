2952cba3bb17db4042c3a86f67f4a89c467b4d59
dutnals <dutnaks77@sookmyung.ac.kr>
2025-10-19 23:44:08 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter);
+
 int main(int argc, char *argv[])
 {
-    int i;
-    int temp;
+    int i = 10;
 
-    for(i = 0; i < 5; i++)
-    {
-        temp = i;
-        printf("temp=%d\n", temp);
-        temp++;
-    }
+    printf("함수 호출전 i=%d\n", i);
+    i = inc(i);
+    printf("함수 호출후 i=%d\n", i);
 
     system("PAUSE");
     return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
